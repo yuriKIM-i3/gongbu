1844795eb75823b69b35d3705f30d2cb6b76dba6
crane93 <youl9094@gmail.com>
2022-01-10 06:50:43 -0500

    add spring boot package

---
+++ gongbu/src/main/java/com/gongbu/gongbu/GongbuApplication.java
@@ -0,0 +1,13 @@
+package com.gongbu.gongbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongbuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongbuApplication.class, args);
+	}
+
+}
---
+++ gongbu/src/test/java/com/gongbu/gongbu/GongbuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongbu.gongbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongbuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
