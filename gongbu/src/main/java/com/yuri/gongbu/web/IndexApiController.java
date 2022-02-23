e199828e5ca80692a4e85183c215ff483430b87f
crane93 <youl9094@gmail.com>
2022-02-23 07:35:38 -0500

    セッションにあるユーザー情報をカスタムアノテーション化

--- gongbu/src/main/java/com/yuri/gongbu/web/IndexApiController.java
+++ gongbu/src/main/java/com/yuri/gongbu/web/IndexApiController.java
@@ -4,13 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
+
+import com.yuri.gongbu.config.auth.dto.SessionUser;
+import com.yuri.gongbu.config.auth.LoginUser;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexApiController{
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("loginedUser", user);
+        
         return "index";
     }
 }
--- gongbu/src/main/java/com/yuri/gongbu/web/LoginApiController.java
+++ gongbu/src/main/java/com/yuri/gongbu/web/LoginApiController.java
@@ -1,11 +1,8 @@
 package com.yuri.gongbu.web;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 
-
-@RequiredArgsConstructor
 @Controller
 public class LoginApiController{
 
