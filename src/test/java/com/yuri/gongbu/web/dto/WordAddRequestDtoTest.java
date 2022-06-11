45b5cf0c3e1e96055583767cb13e0157443a108a
crane93 <youl9094@gmail.com>
2022-06-11 02:24:50 -0400

    入力チェックテストにて、コメントアウト解除

--- src/test/java/com/yuri/gongbu/web/dto/WordAddRequestDtoTest.java
+++ src/test/java/com/yuri/gongbu/web/dto/WordAddRequestDtoTest.java
@@ -11,7 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.io.StringWriter;
-import org.junit.jupiter.api.Disabled;
 
 import com.yuri.gongbu.web.dto.WordAddRequestDto;
 import com.yuri.gongbu.global.GlobalVariable;
@@ -43,120 +42,112 @@ public class WordAddRequestDtoTest {
     }
 
 
-    // @Test
-    // void wordNameUptoMaxLength() throws Exception {
-    //     // given
-    //     String wordName =  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_NAME_MAX_LENGTH;i++) write("a"); }}.toString();
-    //     wordAddRequestDto.setWordName(wordName);
-    //     // when
-    //     validator.validate(wordAddRequestDto, bindingResult);
-    //     // then
-    //     assertNull(bindingResult.getFieldError());
-    // }
+    @Test
+    void wordNameUptoMaxLength() throws Exception {
+        // given
+        String wordName =  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_NAME_MAX_LENGTH;i++) write("a"); }}.toString();
+        wordAddRequestDto.setWordName(wordName);
+        // when
+        validator.validate(wordAddRequestDto, bindingResult);
+        // then
+        assertNull(bindingResult.getFieldError());
+    }
 
-    // @Disabled
-    // @Test
-    // void wordNameOverMaxLength() throws Exception {
-    //     // given
-    //     String wordName =  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_NAME_MAX_LENGTH + 1;i++) write("a"); }}.toString();
-    //     wordAddRequestDto.setWordName(wordName);
-    //     // when
-    //     validator.validate(wordAddRequestDto, bindingResult);
-    //     // then
-    //     assertNotNull(bindingResult.getFieldError());
-    // }
+    @Test
+    void wordNameOverMaxLength() throws Exception {
+        // given
+        String wordName =  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_NAME_MAX_LENGTH + 1;i++) write("a"); }}.toString();
+        wordAddRequestDto.setWordName(wordName);
+        // when
+        validator.validate(wordAddRequestDto, bindingResult);
+        // then
+        assertNotNull(bindingResult.getFieldError());
+    }
 
-    // @Disabled
-    // @Test
-    // void wordPronunciationInputNull() throws Exception {
-    //     // given
-    //     wordAddRequestDto.setWordPronunciation(null);
-    //     // when
-    //     validator.validate(wordAddRequestDto, bindingResult);
-    //     // then
-    //     assertNotNull(bindingResult.getFieldError());
-    // }
+    @Test
+    void wordPronunciationInputNull() throws Exception {
+        // given
+        wordAddRequestDto.setWordPronunciation(null);
+        // when
+        validator.validate(wordAddRequestDto, bindingResult);
+        // then
+        assertNotNull(bindingResult.getFieldError());
+    }
 
-    // @Disabled
-    // @Test
-    // void wordPronunciationUptoMaxLength() throws Exception {
-    //     // given
-    //     String wordPronunciation =  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_PRONUNCIATION_MAX_LENGTH;i++) write("a"); }}.toString();
-    //     wordAddRequestDto.setWordPronunciation(wordPronunciation);
-    //     // when
-    //     validator.validate(wordAddRequestDto, bindingResult);
-    //     // then
-    //     assertNull(bindingResult.getFieldError());
-    // }
+    @Test
+    void wordPronunciationUptoMaxLength() throws Exception {
+        // given
+        String wordPronunciation =  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_PRONUNCIATION_MAX_LENGTH;i++) write("a"); }}.toString();
+        wordAddRequestDto.setWordPronunciation(wordPronunciation);
+        // when
+        validator.validate(wordAddRequestDto, bindingResult);
+        // then
+        assertNull(bindingResult.getFieldError());
+    }
 
-    // @Disabled
-    // @Test
-    // void wordPronunciationOverMaxLength() throws Exception {
-    //     // given
-    //     String wordPronunciation =  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_PRONUNCIATION_MAX_LENGTH + 1;i++) write("a"); }}.toString();
-    //     wordAddRequestDto.setWordPronunciation(wordPronunciation);
-    //     // when
-    //     validator.validate(wordAddRequestDto, bindingResult);
-    //     // then
-    //     assertNotNull(bindingResult.getFieldError());
-    // }
+    @Disabled
+    @Test
+    void wordPronunciationOverMaxLength() throws Exception {
+        // given
+        String wordPronunciation =  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_PRONUNCIATION_MAX_LENGTH + 1;i++) write("a"); }}.toString();
+        wordAddRequestDto.setWordPronunciation(wordPronunciation);
+        // when
+        validator.validate(wordAddRequestDto, bindingResult);
+        // then
+        assertNotNull(bindingResult.getFieldError());
+    }
 
-    // @Disabled
-    // @Test
-    // void wordMeaningInputNull() throws Exception {
-    //     // given
-    //     wordAddRequestDto.setWordMeaning(null);
-    //     // when
-    //     validator.validate(wordAddRequestDto, bindingResult);
-    //     // then
-    //     assertNotNull(bindingResult.getFieldError());
-    // }
+    @Test
+    void wordMeaningInputNull() throws Exception {
+        // given
+        wordAddRequestDto.setWordMeaning(null);
+        // when
+        validator.validate(wordAddRequestDto, bindingResult);
+        // then
+        assertNotNull(bindingResult.getFieldError());
+    }
 
-    // @Disabled
-    // @Test
-    // void wordMeaningUptoMaxLength() throws Exception {
-    //     // given
-    //     String wordMeaning=  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_MEANING_MAX_LENGTH;i++) write("a"); }}.toString();
-    //     wordAddRequestDto.setWordMeaning(wordMeaning);
-    //     // when
-    //     validator.validate(wordAddRequestDto, bindingResult);
-    //     // then
-    //     assertNull(bindingResult.getFieldError());
-    // }
+    @Test
+    void wordMeaningUptoMaxLength() throws Exception {
+        // given
+        String wordMeaning=  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_MEANING_MAX_LENGTH;i++) write("a"); }}.toString();
+        wordAddRequestDto.setWordMeaning(wordMeaning);
+        // when
+        validator.validate(wordAddRequestDto, bindingResult);
+        // then
+        assertNull(bindingResult.getFieldError());
+    }
 
-    // @Disabled
-    // @Test
-    // public void wordMeaningOverMaxLength() throws Exception {
-    //     // given
-    //     String wordMeaning=  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_MEANING_MAX_LENGTH + 1;i++) write("a"); }}.toString();
-    //     wordAddRequestDto.setWordMeaning(wordMeaning);
-    //     // when
-    //     validator.validate(wordAddRequestDto, bindingResult);
-    //     // then
-    //     assertNotNull(bindingResult.getFieldError());
-    // }
+    @Test
+    public void wordMeaningOverMaxLength() throws Exception {
+        // given
+        String wordMeaning=  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_MEANING_MAX_LENGTH + 1;i++) write("a"); }}.toString();
+        wordAddRequestDto.setWordMeaning(wordMeaning);
+        // when
+        validator.validate(wordAddRequestDto, bindingResult);
+        // then
+        assertNotNull(bindingResult.getFieldError());
+    }
 
-    // @Disabled
-    // @Test
-    // public void wordExampleUptoMaxLength() throws Exception {
-    //     // given
-    //     String wordExample=  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_EXAMPLE_MAX_LENGTH;i++) write("a"); }}.toString();
-    //     wordAddRequestDto.setWordExample(wordExample);
-    //     // when
-    //     validator.validate(wordAddRequestDto, bindingResult);
-    //     // then
-    //     assertNull(bindingResult.getFieldError());
-    // }
+    @Test
+    public void wordExampleUptoMaxLength() throws Exception {
+        // given
+        String wordExample=  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_EXAMPLE_MAX_LENGTH;i++) write("a"); }}.toString();
+        wordAddRequestDto.setWordExample(wordExample);
+        // when
+        validator.validate(wordAddRequestDto, bindingResult);
+        // then
+        assertNull(bindingResult.getFieldError());
+    }
 
-    // @Disabled
-    // @Test
-    // public void wordExampleOverMaxLength() throws Exception {
-    //     // given
-    //     String wordExample=  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_EXAMPLE_MAX_LENGTH + 1;i++) write("a"); }}.toString();
-    //     wordAddRequestDto.setWordExample(wordExample);
-    //     // when
-    //     validator.validate(wordAddRequestDto, bindingResult);
-    //     // then
-    //     assertNotNull(bindingResult.getFieldError());
-    // }
+    @Test
+    public void wordExampleOverMaxLength() throws Exception {
+        // given
+        String wordExample=  new StringWriter(){{ for(int i=0;i<GlobalVariable.WORD_EXAMPLE_MAX_LENGTH + 1;i++) write("a"); }}.toString();
+        wordAddRequestDto.setWordExample(wordExample);
+        // when
+        validator.validate(wordAddRequestDto, bindingResult);
+        // then
+        assertNotNull(bindingResult.getFieldError());
+    }
 }
